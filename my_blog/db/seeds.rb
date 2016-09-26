7d36a330156d99b2f0347db67e3a95fb09e32444
Brandon Block <brandon.block@gmail.com>
2016-09-26 17:12:41 -0400

    seed db

--- my_blog/db/seeds.rb
+++ my_blog/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u1 = User.create(email: 'user@example.com', password: 'password')
+u2 = User.create(email: 'user2@example.com', password: 'password')
+
+p1 = u1.posts.create(title: 'First Post', body: 'A Commodore 64')
+p2 = u1.posts.create(title: 'Second Post', body: 'A NES')
+
+p3 = u2.posts.create(title: 'Third Post', body: 'A Atari')
+p4 = u2.posts.create(title: 'Fourth Post', body: 'A Colecovision')
+
+p3.comments.create(body: "This post was terrible", user: u1)
+p4.comments.create(body: "This post was freaking fantastic", user: u1)
